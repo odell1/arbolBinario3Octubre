b5f7fe3201ba672b45c87d058669a765d2496b34
odell1 <pablo.lledo@gmail.com>
2025-10-08 13:01:17 +0200

    d

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        // Primero debemos dilucidar si el árbol va a contener elementos repetidos o no
+        // Las claves o los valores principales del árbol deben ser comparables
+        // Si el árbol va a ser auto-balanceado o no (completo o no completo)
+
+        //Falta por hacer:
+        // - Si es completo o no
+        // - Borrar un nodo (con los tres casos)
+
         int[] valores= {50,30,70,20,40,60,80};
          ArbolBinarioBusqueda abb=new ArbolBinarioBusqueda();
         for(int valor:valores){
@@ -10,5 +18,16 @@ public class App {
 
         System.out.println("Recorrido en orden");
         abb.EnOrden(abb.raiz);
+        
+
+        System.out.println("Recorrido en PreOrden");
+        abb.preOrden(abb.raiz);
+
+        System.out.println("Recorrido en PosOrden");
+        abb.posOrden(abb.raiz);
+
+
+        System.out.println("Valor menor ");
+        System.out.println(abb.encontrarMinimo(abb.raiz));
     }
 }
--- src/ArbolBinarioBusqueda.java
+++ src/ArbolBinarioBusqueda.java
@@ -34,16 +34,93 @@ public class ArbolBinarioBusqueda {
 
         }//insertarRecursivo
 
+        //Recorrido EnOrden (InOrden) Izquierda, raiz, Derecha
         public void EnOrden(Nodo nodo){
             if(nodo!=null){
                 EnOrden(nodo.izquierda);
-                System.out.println("Valor "+nodo.getValor());
+                System.out.println("Valor "+nodo.getValor()+" ");
                 EnOrden(nodo.getDerecha());
             }//if
 
         }//EnOrden
 
+        //Recorrido preOrden raiz, izquierda, derecha
+        public void preOrden(Nodo nodo){
+            if(nodo!=null){
+                System.out.print("Valor "+nodo.getValor()+" ");
+                preOrden(nodo.izquierda);
+                preOrden(nodo.getDerecha());
+            }//if
 
+        }//EnOrden
+
+
+       //Recorrido posOrden  izquierda, derecha, raiz
+        public void posOrden(Nodo nodo){
+            if(nodo!=null){
+                posOrden(nodo.izquierda);
+                posOrden(nodo.getDerecha());
+                System.out.print("Valor "+nodo.getValor()+" ");
+            }//if
+
+        }//EnOrden
+
+////////////////
+/// Búsqueda ojete! debe ser recursivo
+/// Debemos pensar en recursividad!!!!
+/// ////////////////////////
+        public boolean buscar(int valor){
+            return buscarRecursivo(raiz,valor);
+        }
+
+        private boolean buscarRecursivo(Nodo nodo, int valor) {
+           //Primero comprobamos si el nodo es null
+           if (nodo==null) return false;
+            // comprobamos si el nodo es el valor
+           if(nodo.getValor()==valor) return true;
+
+           return valor<nodo.getValor() ? buscarRecursivo(nodo.getIzquierda(), valor) : buscarRecursivo(nodo.getDerecha(), valor);
+           /* if (valor<nodo.getValor()) buscarRecursivo(nodo.getIzquierda(), valor) else buscarRecursivo(nodo.getDerecha(), valor); */
+
+
+        }//buscarRecursivo
+
+//////////////////////////
+/// Buscar el valor mínimo
+//////////////////////////
+    public int encontrarMinimo(Nodo nodo) {
+        return nodo.getIzquierda() == null ? nodo.valor : encontrarMinimo(nodo.getIzquierda());
+    }//encontrarMinimo
+    
+
+
+//////////////////////////
+/// Altura del árbol
+//////////////////////////
+// Sin recursividad
+public int calcularAltura(Nodo nodo)
+{
+    int altura=0;
+    while(nodo!=null){
+        altura++;
+        if(nodo.getIzquierda()!=null){
+            nodo=nodo.getIzquierda();
+        }else{
+            nodo=nodo.getDerecha();
+        }//if
+
+    }//while
+    return altura;
+}//calcularAltura
+
+//Con recursividad
+public int calcularAlturaRecursiva(Nodo nodo){
+    if(nodo==null) return 0;
+    else{
+        return 1 + Math.max(calcularAlturaRecursiva(nodo.getIzquierda()),calcularAlturaRecursiva(nodo.getDerecha()));
+    }//else 
+
+}//calcularAlturaRecursiva
 
 
 
